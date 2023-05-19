d1f13c77c3c73bdd93972a2b26dde8fe0d786636
varsha1n <102849159+varsha1n@users.noreply.github.com>
2023-05-19 11:28:06 +0530

    Delete back directory

--- back/src/models/Users.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-//schema is an object that will define structure of data
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-
-export const UserModel = mongoose.model("users", UserSchema); // table or collection called users
